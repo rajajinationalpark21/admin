bad18447aad136555a5eff852e743780c0a4a772
rajajinationalpark21 <rajajinationalpark513@gmail.com>
2026-09-17 22:21:03 +0530

    fix: add missing React import in InquiryManager causing page crash

--- src/pages/InquiryManager.jsx
+++ src/pages/InquiryManager.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import Header from "../components/common/Header";
 import {
   Box,
